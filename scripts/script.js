f53b92a866145585bbeb8b299709c3ca9380a991
Alejandro Tejeda <alejanafs28@gmail.com>
2025-12-11 16:50:36 -0600

    Added viajes and animate on scroll

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,52 @@
 function updateCountdown() {
-      const target = new Date("December 14, " + new Date().getFullYear() + " 00:00:00").getTime();
-      const now = Date.now();
-      const diff = target - now;
+    const target = new Date("December 14, " + new Date().getFullYear() + " 00:00:00").getTime();
+    const now = Date.now();
+    const diff = target - now;
 
-      if (diff <= 0) {
-        document.getElementById("countdown").textContent = "🎉 It's your day! Happy Birthday! 🎉";
+    if (diff <= 0) {
+        // Show rewind section after countdown ends with a delay
+        console.log("aui")
+        setTimeout(() => {
+            document.getElementById('countdownPage').style.display = 'none';
+            document.getElementById('rewindSection').style.display = 'block';
+            document.getElementById('scrollIndicator').style.display = 'block';
+        }, 2000);
         return;
-      }
+    }
 
-      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const mins = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-      const secs = Math.floor((diff % (1000 * 60)) / 1000);
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const mins = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const secs = Math.floor((diff % (1000 * 60)) / 1000);
 
-      document.getElementById("countdown").textContent =
+    document.getElementById("countdown").textContent =
         `${days}d ${hours}h ${mins}m ${secs}s`;
 }
 
-setInterval(updateCountdown, 1000);
 updateCountdown();
+
+const audio = document.getElementById("audio");
+const btn = document.getElementById("toggleBtn");
+const catImage = document.getElementById("cat-image");
+
+// Click event: play/pause
+btn.addEventListener("click", () => {
+    if (audio.paused) {
+        catImage.src = "assets/hablando.gif";  
+        audio.play();
+        btn.classList.remove("play");
+        btn.classList.add("pause");
+    } else {
+        catImage.src = "assets/hablando.png"; 
+        audio.pause();
+        btn.classList.remove("pause");
+        btn.classList.add("play");
+    }
+});
+
+// When audio finishes
+audio.addEventListener("ended", () => {
+    catImage.src = "assets/hablando.png";     
+    btn.classList.remove("pause");
+    btn.classList.add("play");
+});
